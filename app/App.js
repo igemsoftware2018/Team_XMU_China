acaaa8b5258035ed9341f2cef4c35289704e4ece
XMU-China-2018 <43910749+XMU-China-2018@users.noreply.github.com>
2018-10-17 16:39:37 +0800

    Update App.js

--- app/App.js
+++ app/App.js
@@ -115,36 +115,29 @@ const Navigator = createMaterialTopTabNavigator({
     },
 
 }, {
-    //设置TabNavigator的位置
     tabBarPosition: 'bottom',
-    //是否在更改标签时显示动画
     animationEnabled: true,
-    //是否允许在标签之间进行滑动
     swipeEnabled: true,
-    //按 back 键是否跳转到第一个Tab(首页)， none 为不跳转
     backBehavior: "none",
-    //设置Tab标签的属性
     tabBarOptions: {
-        //Android属性
-        upperCaseLabel: false,//是否使标签大写，默认为true
-        //共有属性
-        showIcon: true,//是否显示图标，默认关闭
-        showLabel: true,//是否显示label，默认开启
-        activeTintColor: '#C173B8',//label和icon的前景色 活跃状态下（选中）
-        inactiveTintColor: 'gray',//label和icon的前景色 活跃状态下（未选中）
-        style: { //TabNavigator 的背景颜色
+        upperCaseLabel: false,
+        showIcon: true,
+        showLabel: true,
+        activeTintColor: '#C173B8',
+        inactiveTintColor: 'gray',
+        style: { 
             backgroundColor: 'white',
             height: 50,
         },
         indicatorStyle: {
             height: 0,
         },
-        labelStyle: {//文字的样式
+        labelStyle: {
             fontSize: 12,
             marginTop: -5,
             marginBottom: 5,
         },
-        iconStyle: {//图标的样式
+        iconStyle: {
             marginBottom: 5,
         }
     },
