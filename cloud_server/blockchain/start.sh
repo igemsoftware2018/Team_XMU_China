86dedca311d774378dbbefc8bf1befd33118b2fa
XMU-China-2018 <43910749+XMU-China-2018@users.noreply.github.com>
2018-10-18 03:22:29 +0800

    Update start.sh

--- cloud_server/blockchain/start.sh
+++ cloud_server/blockchain/start.sh
@@ -1,12 +1,12 @@
-# 启动之前需要手工启动bootnode
+# need start up bootnode in advance
 if [ ! -f bootnode.log ];then
     echo "please run bootnode.sh first"
     exit
 fi
-# 解析本机的ip地址
+
 ip=$(ifconfig|grep inet|grep -v inet6|grep Bcast|awk '{print $2}')
 ip=${ip:5}
-# 解析bootnode地址
+
 bootnode_addr=enode://"$(grep enode bootnode.log|tail -n 1|awk -F '://' '{print $2}'|awk -F '@' '{print $1}')""@$ip:30301"
 if [ "$1" == "" ];then
     echo "node id is empty, please use: start.sh <node_id>";
@@ -15,12 +15,12 @@ fi
 no=$1
 datadir=./
 mkdir -p $datadir
-# 如果启动前需要使用创世块初始化
+
 if [ ! -d "$DIRECTORY" ]; then
     if [ ! -f $datadir/genesis ];then
         echo '{"config": {"chainId": 15, "homesteadBlock": 0, "eip155Block": 0, "eip158Block": 0 }, "coinbase" : "0x0000000000000000000000000000000000000000", "difficulty" : "0x1", "extraData" : "0x", "gasLimit" : "0xffffffff", "nonce" : "0x0000000000000042", "mixhash" : "0x0000000000000000000000000000000000000000000000000000000000000000", "parentHash" : "0x0000000000000000000000000000000000000000000000000000000000000000", "timestamp" : "0x00", "alloc": { } }' > $datadir/genesis
     fi
     /home/ubuntu/eth/go-ethereum/build/bin/geth --datadir $datadir/$no init $datadir/genesis
 fi
-# 运行geth，启动console
+
 /home/ubuntu/eth/go-ethereum/build/bin/geth --preload /home/ubuntu/test/autominer.js --datadir $datadir/$no --networkid 11100 --ipcdisable --port 619$no --rpc --rpcapi "db,eth,net,web3,txpool,personal" --rpcaddr $ip --rpccorsdomain "*" --rpcport 81$no --bootnodes $bootnode_addr console
